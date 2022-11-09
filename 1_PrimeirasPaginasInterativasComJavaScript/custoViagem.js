6a406ccf3fbd624df5d3034ca3d12a0323710984
Lucena <jeffersoonlucena@gmail.com>
2022-11-09 18:28:32 -0300

    Adição primeiro exercício

---
+++ 1_PrimeirasPaginasInterativasComJavaScript/custoViagem.js
@@ -0,0 +1,16 @@
+// Faça um programa que calcule o custo total de uma viagem.
+
+// Teremos de receber como variáveis, distância em KM, preço combustível e
+// gasto de combustível por litro do veículo.
+// Depois de receber as variáveis, calcular o custo e imprimir no console.
+
+// Nessa V1.0 utilizaremos valores fixos para desenvolvermos a lógica.
+
+    distancia = 180;
+    kmPorLitro = 10;
+    precoCombustivel = 5.99;
+    custoViagem = 0;
+
+    custoViagem = (distancia / kmPorLitro) * precoCombustivel
+
+    console.log(custoViagem);
