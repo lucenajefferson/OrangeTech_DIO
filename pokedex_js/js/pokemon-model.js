f990e3c1a88e504a693fc5af3762eeedd0de96a0
Jefferson Lucena <jeffersoonlucena@gmail.com>
2022-12-07 22:02:58 -0300

    PokedexFinalizada

---
+++ pokedex_js/js/pokemon-model.js
@@ -0,0 +1,8 @@
+// Criação da estrutura de um pokemon, com número, nome, tipo ou tipos (que serão alocados dentro do vetor) e a sua imagem.
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+}
